9f0e38e9d33b4033388f2addf8a18d4cf6adef8a
Jaskaran Singh <jasingh784@gmail.com>
2021-04-11 12:12:19 -0700

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import MapView from 'react-native-maps';
+import * as Location from 'expo-location';
 
 export default function App() {
+
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [region, setRegion] = useState(null)
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.watchPositionAsync({accuracy: Location.Accuracy.BestForNavigation, timeInterval: 5000}, locationUpdated);
+      // setLocation(location);
+      // console.log(location)
+      // setRegion({
+      //   latitude: location.coords.latitude,
+      //   longitude: location.coords.longitude,
+      //   latitudeDelta: 0.0922,
+      //   longitudeDelta: 0.0421,
+      // })
+    })();
+
+  }, [])
+
+  const locationUpdated = (locObject) => {
+    console.log('inside locationupdated')
+    console.log(locObject)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <MapView  style={styles.map}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        region={region}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  map: {
+    flex: 1,
+  }
 });
