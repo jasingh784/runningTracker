2000506eb4083a3449c5238e1f87b0ba9de2b994
jasingh784 <48878742+jasingh784@users.noreply.github.com>
2021-04-11 20:13:41 -0700

    working on adding firebase

--- App.js
+++ App.js
@@ -6,6 +6,22 @@ import AccountScreen from './screens/AccountScreen';
 import RouteScreen from './screens/RouteScreen';
 import NewRunStackScreen from './screens/NewRunStackScreen';
 
+import firebase from 'firebase/app';
+import "firebase/auth";
+import "firebase/firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDrA7w5ohDIeww_hrw_FhqX-mhAbiEHMcA",
+  authDomain: "runningtracker-818ae.firebaseapp.com",
+  projectId: "runningtracker-818ae",
+  storageBucket: "runningtracker-818ae.appspot.com",
+  messagingSenderId: "175313089708",
+  appId: "1:175313089708:web:7a10911a4612f3b05edb09",
+  measurementId: "G-YYP1WGTP3Y"
+};
+
+firebase.initializeApp(firebaseConfig);
+
 const Tab = createBottomTabNavigator()
 
 export default function App() {
